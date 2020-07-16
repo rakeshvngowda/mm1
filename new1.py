2687a861b28506b6e8be3f7f8c78d2f0d4640f9d
rakesh <rakeshvngowda@gmail.com>
2020-07-16 08:45:22 +0530

    i am added decorator

--- new1.py
+++ new1.py
@@ -1,4 +1,12 @@
-def add(x, y):
+def decor(func):
+    def inner(x, y):
+        if y == 0:
+            print('number is not divisale by zero')
+        else:
+            return func(x, y)
+    return inner
+
+@decor
+def div(x, y):
     return x / y 
-    
     
